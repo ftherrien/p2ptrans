14eb3a42c58fc7a83b0e6e88f4561d5662ecfb9b
Ash Pera <aepera@mines.edu>
2020-05-30 11:29:22 -0600

    made test have __main__ check

--- simpletest/test.py
+++ simpletest/test.py
@@ -5,37 +5,39 @@ from pylada.crystal import read
 from test_utils import *
 import pickle
 
-bcc = read.poscar(path.join('ref','BCC_Fe_POSCAR'))
-fcc = read.poscar(path.join('ref','FCC_Fe_POSCAR'))
 
-outdir = 'results'
-if not path.exists(outdir):
-   makedirs(outdir) 
+if __name__ == "__main__":
+    bcc = read.poscar(path.join('ref','BCC_Fe_POSCAR'))
+    fcc = read.poscar(path.join('ref','FCC_Fe_POSCAR'))
 
-# n_steps, (tmat, dispStruc, vec_classes), outdir, display
-tmat, dispStruc, vec_classes = p2p.findMatching(bcc, fcc, 100, outdir=outdir)
-p2p.produceTransition(60, tmat, dispStruc, vec_classes, outdir, False)
+    outdir = 'results'
+    if not path.exists(outdir):
+        makedirs(outdir) 
 
-# check that tmats are close
-pickle.dump( tmat, open(path.join(outdir,'tmat.pickle'), 'wb') )
-ref_tmat = pickle.load(open(path.join('ref','tmat.pickle'), 'rb'))
-if not compare_tmats(ref_tmat, tmat, tol=1e-4):
-    print('FAILED TMATS!!!!')
-    exit()
+    # n_steps, (tmat, dispStruc, vec_classes), outdir, display
+    tmat, dispStruc, vec_classes = p2p.findMatching(bcc, fcc, 100, outdir=outdir)
+    p2p.produceTransition(60, tmat, dispStruc, vec_classes, outdir, False)
 
-
-# then verifiy that produced poscars are close to reference
-ref_structs = structs_in_dir(path.join('ref', 'TransPOSCARS'))
-produced_structs = structs_in_dir(path.join('results', 'TransPOSCARS'))
-
-for ref_struct, res_struct in zip(ref_structs, produced_structs):
-    if not compare_structs(ref_struct, res_struct, tol=1e-4):
-        print('FAILED!!!!')
-        print()
-        print(ref_struct, 'DNE', res_struct)
+    # check that tmats are close
+    pickle.dump( tmat, open(path.join(outdir,'tmat.pickle'), 'wb') )
+    ref_tmat = pickle.load(open(path.join('ref','tmat.pickle'), 'rb'))
+    if not compare_tmats(ref_tmat, tmat, tol=1e-4):
+        print('FAILED TMATS!!!!')
         exit()
 
-print('Succeded!')
+
+    # then verifiy that produced poscars are close to reference
+    ref_structs = structs_in_dir(path.join('ref', 'TransPOSCARS'))
+    produced_structs = structs_in_dir(path.join('results', 'TransPOSCARS'))
+    
+    for ref_struct, res_struct in zip(ref_structs, produced_structs):
+        if not compare_structs(ref_struct, res_struct, tol=1e-4):
+            print('FAILED!!!!')
+            print()
+            print(ref_struct, 'DNE', res_struct)
+            exit()
+
+     print('Succeded!')
 
 
 
