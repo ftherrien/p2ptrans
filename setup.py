83bff5480cac8f7ff9fa99c64572df8ce7fda311
Ash Pera <aepera@mines.edu>
2021-02-24 13:32:43 -0700

    added lgfortran

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from numpy.distutils.core import setup, Extension
 sources = ['source/lapjv.hpp', 'source/lapjv.cpp', 'source/lapjv.f90', 'source/lap.f90', 'source/utils.f90', 'source/tiling.f90', 'source/potential.f90', 'source/transform.f90']
 functions = ["only:","munkres", "free_trans", "rot_mat", "center", "eye", "norm", "split", "det", "sort", "sphere", "circle", "distance", "derivative", "intoptimization", "fastoptimization", "lapjv", ":"]
 
-mod = Extension(name='p2ptrans.fmodules', sources=sources, extra_f90_compile_args=["-fopenmp","-O3"], extra_link_args=['-lgomp'], f2py_options=functions)
+mod = Extension(name='p2ptrans.fmodules', sources=sources, extra_f90_compile_args=["-fopenmp","-O3"], extra_link_args=['-lgomp', '-lgfortran'], f2py_options=functions)
 
 setup(name='p2ptrans',
       version='2.0.2',
