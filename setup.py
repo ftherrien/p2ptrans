904594f67195d17e5422e217ebb5b61b55b22bc6
ftherrien <felix.therrien@gmail.com>
2021-03-11 11:20:28 -0500

    Unsuccessful attempt at cmake

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
-import setuptools
-from numpy.distutils.core import setup, Extension
-
-sources = ['source/lapjv.hpp', 'source/lapjv.cpp', 'source/lapjv.f90', 'source/lap.f90', 'source/utils.f90', 'source/tiling.f90', 'source/potential.f90', 'source/transform.f90']
-functions = ["only:","munkres", "free_trans", "rot_mat", "center", "eye", "norm", "split", "det", "sort", "sphere", "circle", "distance", "derivative", "intoptimization", "fastoptimization", "lapjv", ":"]
-
-mod = Extension(name='p2ptrans.fmodules', sources=sources, extra_f90_compile_args=["-fopenmp","-O3"], extra_link_args=['-lgomp', '-lgfortran'], f2py_options=functions)
+from skbuild import setup
 
 setup(name='p2ptrans',
       version='2.0.2',
@@ -14,7 +8,6 @@ setup(name='p2ptrans',
       author_email='felix.therrien@gmail.com',
       license='MIT',
       packages=['p2ptrans'],
-      ext_modules=[mod],
       python_requires='>=3',
       install_requires=[
           'numpy',
